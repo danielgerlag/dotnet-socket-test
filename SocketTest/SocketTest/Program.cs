dda2a570d0e40f884d6f7cf627f69ad52e1b6793
Daniel Gerlag <daniel@gerlag.ca>
2017-09-12 19:31:01 -0700

    .

--- SocketTest/SocketTest/Program.cs
+++ SocketTest/SocketTest/Program.cs
@@ -1,54 +1,58 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SocketTest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            static void Main(string[] args)
+            var cancel = new CancellationTokenSource();
+            var listener = new TcpListener(IPAddress.Any, 17000);
+            listener.Start();
+
+            Task.Factory.StartNew(async () =>
             {
-                var cancel = new CancellationTokenSource();
-                var listener = new TcpListener(IPAddress.Any, 700);
-                listener.Start();
-
-                Task.Factory.StartNew(async () =>
+                while (!cancel.IsCancellationRequested)
                 {
-                    while (!cancel.IsCancellationRequested)
+                    try
                     {
-                        try
-                        {
-                            var client = await listener.AcceptTcpClientAsync();
-                            client.Dispose();
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Server - {ex.Message}");
-                        }
+                        var client = await listener.AcceptTcpClientAsync();
+                        client.Dispose();
                     }
-                });
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Server - {ex.Message}");
+                    }
+                }
+            });
 
-                Task.Factory.StartNew(async () =>
+            Task.Factory.StartNew(async () =>
+            {
+                while (!cancel.IsCancellationRequested)
                 {
-                    while (!cancel.IsCancellationRequested)
+                    try
                     {
-                        try
-                        {
-                            var client = new TcpClient();
-                            await client.ConnectAsync("localhost", 700);
-                            client.Dispose();
-                            await Task.Delay(100, cancel.Token);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Client - {ex.Message}");
-                        }
+                        var client = new TcpClient();
+                        await client.ConnectAsync("localhost", 17000);
+                        client.Dispose();
+                        await Task.Delay(100, cancel.Token);
                     }
-                });
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Client - {ex.Message}");
+                    }
+                }
+            });
 
-                Console.ReadLine();
-                cancel.Cancel();
-                listener.Stop();
-            }
+            Console.ReadLine();
+            cancel.Cancel();
+            listener.Stop();
+        }
+
     }
 }
